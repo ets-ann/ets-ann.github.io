5855926c92cb176a03a75a7f1f2e835252325fa6
ets-ann <160623733+ets-ann@users.noreply.github.com>
2024-02-20 17:52:54 +0200

    add paypal google pay core

---
+++ advanced-integration/paypal-api.js
@@ -0,0 +1,130 @@
+import fetch from "node-fetch";
+
+// set some important variables
+const { PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET, PAYPAL_MERCHANT_ID , BASE_URL } = process.env;
+// const sandbox = "https://api-m.sandbox.paypal.com";
+// const production = "https://api-m.paypal.com";
+const base = `${BASE_URL}`
+
+// call the create order method
+export async function createOrder() {
+  const purchaseAmount = "0.10"; // TODO: pull prices from a database
+  const accessToken = await generateAccessToken();
+  const url = `${base}/v2/checkout/orders`;
+  const response = await fetch(url, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify({
+      intent: "CAPTURE",
+      purchase_units: [
+        {
+          amount: {
+            breakdown: {
+              item_total: {
+                value: purchaseAmount,
+                currency_code: "USD"
+              }},
+            value: purchaseAmount,
+            currency_code: "USD"
+          },
+          payee: {
+            merchant_id: PAYPAL_MERCHANT_ID,
+          },
+          items: [{
+            quantity: "1",
+            name: "Donation",
+            unit_amount: {
+              "value": purchaseAmount,
+              "currency_code": "USD"
+            },
+            category: "DONATION" // changing to DIGITAL_GOODS resolves the issue:
+                                 // The 'API caller' and/or 'payee' is not setup to be able to process google pay.
+                                 // Please contact your Account Manager.
+          }]
+        },
+      ],
+      payment_source: {
+        google_pay:{
+          attributes:{
+            verification: {
+              method: 'SCA_WHEN_REQUIRED',
+            },
+          }
+        }
+      }
+    }),
+  });
+
+  return handleResponse(response);
+}
+
+// capture payment for an order
+export async function capturePayment(orderId) {
+  const accessToken = await generateAccessToken();
+  const url = `${base}/v2/checkout/orders/${orderId}/capture`;
+  const response = await fetch(url, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  return handleResponse(response);
+}
+
+// generate access token
+export async function generateAccessToken() {
+  const auth = Buffer.from(PAYPAL_CLIENT_ID + ":" + PAYPAL_CLIENT_SECRET).toString("base64");
+  const response = await fetch(`${base}/v1/oauth2/token`, {
+    method: "post",
+    body: "grant_type=client_credentials",
+    headers: {
+      Authorization: `Basic ${auth}`,
+    },
+  });
+  const jsonData = await handleResponse(response);
+  return jsonData.access_token;
+}
+
+// generate client token
+export async function generateClientToken() {
+  const accessToken = await generateAccessToken();
+  const response = await fetch(`${base}/v1/identity/generate-token`, {
+    method: "post",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Accept-Language": "en_US",
+      "Content-Type": "application/json",
+    },
+  });
+  console.log('response', response.status)
+  const jsonData = await handleResponse(response);
+  return jsonData.client_token;
+}
+
+async function handleResponse(response) {
+  if (response.status === 200 || response.status === 201) {
+    return response.json();
+  }
+
+  const errorMessage = await response.text();
+  throw new Error(errorMessage);
+}
+
+export async function getOrder(orderId) {
+  const accessToken = await generateAccessToken();
+  const url = `${base}/v2/checkout/orders/${orderId}`;
+  const response = await fetch(url, {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  return handleResponse(response);
+}
---
+++ advanced-integration/server.js
@@ -0,0 +1,69 @@
+import "dotenv/config";
+import express from "express";
+import * as paypal from "./paypal-api.js";
+const {PORT = 8888} = process.env;
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+// render checkout page with client id & unique client token
+app.get("/", async (req, res) => {
+  const clientId = process.env.PAYPAL_CLIENT_ID, merchantId = process.env.PAYPAL_MERCHANT_ID;
+  const clientSecret = process.env.PAYPAL_CLIENT_SECRET;
+  try {
+    if (!clientId || !merchantId ||  !clientSecret){
+      throw new Error("Client Id or App Secret or Merchant Id is missing.");
+    }
+    const clientToken = await paypal.generateClientToken();
+    console.log(clientToken);
+    res.render("checkout", { clientId, clientToken, merchantId });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+// create order
+app.post("/api/orders", async (req, res) => {
+  try {
+    const order = await paypal.createOrder();
+    res.json(order);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+// Get order
+app.post("/api/orders/:orderID", async (req, res) => {
+  const { orderID } = req.params;
+  try {
+    const order = await paypal.getOrder(orderID);
+    res.json(order);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+// capture payment
+app.post("/api/orders/:orderID/capture", async (req, res) => {
+  const { orderID } = req.params;
+  try {
+    const captureData = await paypal.capturePayment(orderID);
+    res.json(captureData);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+// health check
+app.get("/check" ,(req,res) => {
+  res.json({
+    message: "ok",
+    env: process.env.NODE_ENV, 
+    baseUrl: process.env.BASE_URL
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}/`);
+});
